3622372c97b04eeb5b0980678a2714e3040f7eda
arrietayesleidis <arrietayesleidis@gmail.com>
2025-04-03 18:51:48 -0500

    Update Main.java

--- lista.java/src/listas.java/Main.java
+++ lista.java/src/listas.java/Main.java
@@ -12,9 +12,7 @@ import Formulario.FormularioUsuarios;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
                    
         java.awt.EventQueue.invokeLater(new Runnable() {
